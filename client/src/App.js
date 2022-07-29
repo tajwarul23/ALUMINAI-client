81cc2e704acc6b7cd458fae9f778daf1caeabff5
tajwarul23 <tajwarulchowdhury@gmail.com>
2022-07-29 16:08:27 +0600

    2nd-day

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-
+import logo from './logo.svg';
 import './App.css';
-import {Routes, Route, useNavigate} from 'react-router-dom';
-import Login from './Components/Login';
-import Home from './Container/Home.jsx'
 
 function App() {
   return (
-    <Routes>
-      <Route path='login' element={<Login/>}/>
-      <Route path='/*' element={<Home/>}/>
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter as Router
-  
-} from "react-router-dom";
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { Router } from 'express';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App></App>
-  </Router>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
